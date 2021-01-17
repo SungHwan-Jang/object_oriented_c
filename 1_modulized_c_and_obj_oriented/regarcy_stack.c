cdab1fc2cd72a2114c4dce5f1ecc10846a6dde52
hwan <36683832+SungHwan-Jang@users.noreply.github.com>
2021-01-18 00:49:02 +0900

    c_object_oriented_init1

---
+++ 1_modulized_c_and_obj_oriented/regarcy_stack.c
@@ -0,0 +1,36 @@
+//
+// Created by redmk on 2021-01-17.
+//
+#if 0
+
+#include "regarcy_stack.h"
+#include "stdio.h"
+
+
+int buf[16];
+int top = 0;
+
+static bool isStackFull(void){
+    return top == sizeof (buf) / sizeof (int);
+}
+
+static bool isStackEmpty(void){
+    return top == 0;
+}
+
+bool push(int val){
+    if(isStackFull())
+        return false;
+
+    buf[top++] = val;
+    return true;
+}
+bool pop(int* pRef){
+    if(isStackEmpty())
+        return false;
+
+    *pRef = buf[--top];
+    return true;
+}
+
+#endif
---
+++ 1_modulized_c_and_obj_oriented/step2.c
@@ -0,0 +1,4 @@
+//
+// Created by redmk on 2021-01-18.
+//
+
