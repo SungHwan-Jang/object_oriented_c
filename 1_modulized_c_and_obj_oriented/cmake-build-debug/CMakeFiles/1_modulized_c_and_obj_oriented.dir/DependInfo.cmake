ccb1577f2582f42f2da79d5a295636d37065ff33
hwan <36683832+SungHwan-Jang@users.noreply.github.com>
2021-01-20 00:05:41 +0900

    step1 c_obj_oriented

---
+++ 1_modulized_c_and_obj_oriented/cmake-build-debug/CMakeFiles/1_modulized_c_and_obj_oriented.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/object_oriented_c/object_oriented_c/1_modulized_c_and_obj_oriented/main.c" "E:/object_oriented_c/object_oriented_c/1_modulized_c_and_obj_oriented/cmake-build-debug/CMakeFiles/1_modulized_c_and_obj_oriented.dir/main.c.obj"
+  "E:/object_oriented_c/object_oriented_c/1_modulized_c_and_obj_oriented/regarcy_stack.c" "E:/object_oriented_c/object_oriented_c/1_modulized_c_and_obj_oriented/cmake-build-debug/CMakeFiles/1_modulized_c_and_obj_oriented.dir/regarcy_stack.c.obj"
+  "E:/object_oriented_c/object_oriented_c/1_modulized_c_and_obj_oriented/step1.c" "E:/object_oriented_c/object_oriented_c/1_modulized_c_and_obj_oriented/cmake-build-debug/CMakeFiles/1_modulized_c_and_obj_oriented.dir/step1.c.obj"
+  "E:/object_oriented_c/object_oriented_c/1_modulized_c_and_obj_oriented/step2.c" "E:/object_oriented_c/object_oriented_c/1_modulized_c_and_obj_oriented/cmake-build-debug/CMakeFiles/1_modulized_c_and_obj_oriented.dir/step2.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 1_modulized_c_and_obj_oriented/module.h
@@ -0,0 +1,14 @@
+//
+// Created by redmk on 2021-01-20.
+//
+
+#ifndef INC_1_MODULIZED_C_AND_OBJ_ORIENTED_MODULE_H
+#define INC_1_MODULIZED_C_AND_OBJ_ORIENTED_MODULE_H
+
+#include "stdbool.h"
+
+#define STEP0   false
+#define STEP1   false
+#define STEP2   true
+
+#endif //INC_1_MODULIZED_C_AND_OBJ_ORIENTED_MODULE_H
